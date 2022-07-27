a281640c3eb77831fa098c15def445f6befc6b35
shankar G S <shanakargs.xworkz@gmail.com>
2022-07-27 23:59:37 +0530

    creating the new jar file, kissanJam package , jam,vendor,jar create- export-java runable file,lanching configuration-project name,Export destination- add file name,cmd-java -projectname jarfilename

---
+++ JDBC jar/Jar/src/com/xworkz/KissanJam/Jam.java
@@ -0,0 +1,11 @@
+package com.xworkz.KissanJam;
+
+public class Jam {
+
+	public static void main(String[] args) {
+		Vendor vendor =new Vendor();
+		vendor.getname();
+		System.out.println(vendor.getname());
+	}
+
+}
---
+++ JDBC jar/Jar/src/com/xworkz/KissanJam/Vendor.java
@@ -0,0 +1,12 @@
+package com.xworkz.KissanJam;
+
+public class Vendor {
+
+	public Vendor() {
+		System.out.println("creating Vendor");
+	}
+	String getname() {
+		return "Kissanjam";
+	}
+
+}

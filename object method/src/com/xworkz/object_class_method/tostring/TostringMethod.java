ae474b408627dabb0a5639420c6e2950ad341ad9
shankar G S <shanakargs.xworkz@gmail.com>
2022-08-01 10:40:21 +0530

    remove folder

--- object method/src/com/xworkz/object_class_method/tostring/TostringMethod.java
+++
@@ -1,9 +0,0 @@
-package com.xworkz.object_class_method.tostring;
-
-public class TostringMethod {
-public static void main(String[] args) {
-	Window window=new Window("Glass", 90.00, 50.00);
-	
-	System.out.println(window);
-}
-}
